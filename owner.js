2a2bfb527c09696a9858cd0d4c9b80bfa57d2303
francescacoo@gmail.com <francescacoo@gmail.com>
2018-10-28 19:05:16 +0100

    mapping

--- owner.js
+++ owner.js
@@ -1,31 +1,31 @@
 module.exports=class Owner{
     constructor(name, district, pets, preferredDays){
-        this.name=name
-        this.district=district
-        this.pets=pets
-        this.preferreDays=preferredDays     
+        this.name=name;
+        this.district=district;
+        this.pets=pets;
+        this.preferreDays=preferredDays;     
     }
 
     static create({name,district,pets,preferredDays}){
-        const owner = new Owner(name,district)
-        owner.pets=pets.map(Owner.create)
-        owner.preferreDays=preferredDays.map(Owner.create)
-        return owner
-        
+        const owner = new Owner(name,district);
+        owner.pets=pets.map(Owner.create);
+        owner.preferreDays=preferredDays.map(Owner.create);
+        return owner;
+
     }
 
   
     info() {
-        console.log(this.name, ' is located in', this.district, ' and owns', this.pets)
+        console.log(this.name, ' is located in', this.district, ' and owns', this.pets);
     }
 
     preferred_day(){
-        console.log(this.name, 'available on: ')
+        console.log(this.name, 'available on: ');
         this.preferredDays.forEach(function(item,index,array) {
-            console.log(item)
+            console.log(item);
         });
     }
 
 
-}
+};
 
